40c221089dd78af2039f10b4a21ae5a5d753e883
hubramax <hubramax@students.zhaw.ch>
2024-08-08 00:04:59 +0200

    Scrap nextJs and use react with mui

--- src/pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import { appWithTranslation } from 'next-i18next';
-import '../styles/globals.css';
-import type { AppProps } from 'next/app';
-
-function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
-}
-
-export default appWithTranslation(MyApp);
