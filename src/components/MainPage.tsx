83ad79aca255c1fb98096df9a0f45d494068b706
hubramax <hubramax@students.zhaw.ch>
2024-08-08 00:33:26 +0200

    Base structure

---
+++ src/components/MainPage.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import { ReactComponent as Logo } from '../images/logo.svg';
+
+export function MainPage(): React.ReactElement {
+    return (
+        <body>
+            <Box className='welcomeContent'>
+            </Box>
+        </body>
+    );
+}
