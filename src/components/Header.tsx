247699e43e109b0ac55a2811b9246efbc2aafb0d
hubramax <hubramax@students.zhaw.ch>
2024-08-08 22:02:27 +0200

    Added landing section

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,12 +4,47 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
-import { ReactComponent as Logo } from '../images/logo.svg';
+import {ReactComponent as Logo} from '../images/logo.svg';
+import {useEffect} from 'react';
 
 export function Header(): React.ReactElement {
+    // give header css class when scroll
+    useEffect(() => {
+        const header = document.querySelector('header');
+        const muiToolBarRoot = document.querySelector('.MuiToolbar-root');
+        const hamburger = document.querySelector('.MuiSvgIcon-root');
+
+        const handleScroll = () => {
+            const maxScroll = 60; // The scroll amount at which the header becomes fully opaque
+            let scrollPosition = window.scrollY;
+
+            if (scrollPosition > maxScroll) {
+                scrollPosition = maxScroll;
+            }
+
+            const opacity = scrollPosition / maxScroll;
+            header?.style.setProperty('background-color', `rgba(21, 21, 21, ${opacity})`);
+            header?.classList.toggle('scrolled', scrollPosition > 40);
+            muiToolBarRoot?.classList.toggle('scrolled', scrollPosition > 40);
+            hamburger?.classList.toggle('scrolled', scrollPosition > 40);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        // Set the transition effect for the background color
+        if (header) {
+            header.style.transition = 'background-color 0.5s';
+        }
+
+        // Cleanup function to remove the event listener
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     return (
         <header>
-            <Box sx={{ flexGrow: 1 }}>
+            <Box sx={{flexGrow: 1}}>
                 <AppBar position="static" color={'transparent'}>
                     <Toolbar>
                         <Logo className={'logo'}/>
@@ -18,9 +53,9 @@ export function Header(): React.ReactElement {
                             edge="start"
                             color="inherit"
                             aria-label="menu"
-                            sx={{ mr: 2 }}
+                            sx={{mr: 2}}
                         >
-                            <MenuIcon />
+                            <MenuIcon/>
                         </IconButton>
                     </Toolbar>
                 </AppBar>
--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,15 +1,35 @@
 import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import { ReactComponent as Logo } from '../images/logo.svg';
+import {ReactComponent as Logo} from '../images/logo.svg';
+import {Typography} from '@mui/material';
 
 export function MainPage(): React.ReactElement {
     return (
         <body>
             <Box className='welcomeContent'>
+                <Box className='title-container'>
+                    <Logo className='main-page-logo'/>
+                    <Typography className='subtitle fade-in' color='white' variant='h3'>Liebenfelser Consulting
+                    Group</Typography>
+                    <Typography className='description fade-in' color='white' variant='body1'>Our commitment to excellence in
+                    consulting is quantifiable in results: over 500 successful projects, each unique and unrepeatable.
+                    With Liebenfelser Consulting Group, we have fulfilled our promises, and we could not be more proud
+                    of the outcomes.</Typography>
+                </Box>
+            </Box>
+            <Box className='content'>
+                <Box className='section'>
+                    <Box className='sectionTitle'>About Us</Box>
+                    <Box className='sectionContent'>
+                        <Box className='sectionContentText'>
+                            <p>
+                            We are a group of students from the University of Washington who are passionate about
+                            technology and its applications in the real world. We are excited to work on this project
+                            and hope to make a positive impact on the world.
+                            </p>
+                        </Box>
+                    </Box>
+                </Box>
             </Box>
         </body>
     );
