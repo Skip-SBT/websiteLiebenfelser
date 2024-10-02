8826b0c9aa98ba3edbbf012eff5b95828dfde748
hubramax <hubramax@students.zhaw.ch>
2024-10-02 21:42:56 +0200

    Added new logos and added subsections

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -2,28 +2,54 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import {ReactComponent as Logo} from '../images/logo.svg';
 import {Typography} from '@mui/material';
-import target from '../images/target.jpg';
+
 export function MainPage(): React.ReactElement {
     return (
         <body>
             <Box className='welcomeContent'>
                 <Box className='title-container'>
                     <Logo className='main-page-logo'/>
-                    <Typography className='subtitle fade-in' color='white' variant='h3'>Liebenfelser Consulting
+                    <Typography className='subtitle fade-in' color='black' variant='h3'>Liebenfelser Consulting
                     Group</Typography>
-                    <Typography className='description fade-in' color='white' variant='body1'>Our commitment to excellence in
-                    consulting is quantifiable in results: over 500 successful projects, each unique and unrepeatable.
-                    With Liebenfelser Consulting Group, we have fulfilled our promises, and we could not be more proud
+                    <Typography className='description fade-in' color='black' variant='body1'>Our commitment to
+                    excellence in
+                    consulting is quantifiable in results: over 500 successful projects, each unique and
+                    unrepeatable.
+                    With Liebenfelser Consulting Group, we have fulfilled our promises, and we could not be more
+                    proud
                     of the outcomes.</Typography>
                 </Box>
             </Box>
             <Box className='content'>
                 <Box className='section'>
-                    <Box className='section-left'>
-                        About us
+                    <Box className='section-title'>
+                        <Typography variant='h3'>Über uns</Typography>
                     </Box>
-                    <Box className='section-right'>
-                        <img src={target} alt={''}/>
+                    <Box className='section-description'>
+                        <Typography className='section-text' variant='body1'>Die Liebenfelser Group AG besteht aus
+                            drei Geschäftsfeldern:
+                        <br/>
+                        <br/>
+                            Consulting: Strategie Entwicklung und Implementierung
+                        <br/>
+                            Familiy Office: Vermögensberatung<br/>
+                            Immobilien: Entwicklung und Verkauf</Typography>
+                    </Box>
+
+                    <Box className='section'>
+                        <Box className='section-description'>
+                            <Typography className='section-text' variant='body1'>Die Liebenfelser Group AG besteht aus
+                                drei Geschäftsfeldern:
+                            <br/>
+                                Consulting: Strategie Entwicklung und Implementierung
+                            <br/>
+                                Familiy Office: Vermögensberatung<br/>
+                                Immobilien: Entwicklung und Verkauf</Typography>
+                        </Box>
+                        <Box className='section-title'>
+                            <Typography variant='h3'>Vision</Typography>
+                        </Box>
+
                     </Box>
                 </Box>
             </Box>
